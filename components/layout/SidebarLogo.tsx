1b107046eb68f4f9ec4018a488c70ae6673b8c1f
Dhwanil03 <iamdhwanilrokad@gmail.com>
2024-03-21 22:13:25 +0530

    first commit

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router"
+import { BsTwitterX } from "react-icons/bs";
+
+
+const SidebarLogo = () => {
+    const router = useRouter();
+  return (
+    <div onClick={() => router.push('/')}
+         className=" rounded-full h-14 w-14 p-4 flex tems-center justify-center
+                     hover:bg-neutral-900 cursor-pointer transition ">
+         <BsTwitterX size={28} color="white"/>
+    </div>
+  )
+}
+
+export default SidebarLogo
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "@/components/Layout";
+import LoginModals from "@/components/modals/LoginModals";
+import RegisterModals from "@/components/modals/RegisterModals ";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+import EditModal from "@/components/modals/EditModal";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster/>
+    <EditModal/>  
+    <RegisterModals/>
+     <LoginModals/>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
 }
